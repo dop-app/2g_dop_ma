e563327968c119fc487122ac1f6eaf2364453519
oacastillol <oacastillol@unal.edu.co>
2018-04-17 02:00:41 -0500

    Se agregan caracteristicas a la lista

--- components/PickerList.js
+++ components/PickerList.js
@@ -2,26 +2,31 @@ import React from 'react';
 import { View,Text,Picker,StyleSheet} from 'react-native';
 
 
-class PickerList extends React.Component {
+export default class PickerList extends React.Component {
     constructor(props){
 	super(props),
-	this.state = {text:''};
-    }
-    updateText = (text) => {
-	this.setState({text:text});
+	this.state = {textSelect:'a',textList:['a','b','c']};
     }
     render() {
+	let itemsPicker = this.state.textList.map((v,i) =>{
+	    return <Picker.item  key={i} value={v} label={v}/>;
+	} );
 	return(
 	    <View>
-	      <Picker sleectedValue={this.state.text}
-		      onValueChange={this.updateText} >
-		<Picker.Item label="Test" value="1"/>
-		<Picker.Item label="prueba" value="2"/>
+	      <Picker sleectedValue={this.state.textSelect}
+		      style={{height:50,width:100}}
+		      onValueChange={(text) =>this.setState({textSelect:text}) } >
+		{itemsPicker}
 	      </Picker>
-	      <Text>{this.state.text}</Text>
+	      <Text style = {styles.text}>{this.state.textSelect}</Text>
 	    </View>
 	);
     }    
 }
-export default PickerList;
-
+const styles = StyleSheet.create({
+    text: {
+	fontSize: 30,
+	alignSelf: 'center',
+	color: 'red'
+    }
+})
