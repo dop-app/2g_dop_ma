75f859c92359b7bb9884493977dfb3c4f4f85d83
oacastillol <oacastillol@unal.edu.co>
2018-05-21 15:54:36 -0500

    se intenta agregar conexion al back

--- redux/reducers/auth.js
+++ redux/reducers/auth.js
@@ -1,15 +1,22 @@
 import { request } from 'graphql-request';
 import { USER_TOKEN } from '../graphql/Users';
 
-const endpoint = 'ip/graphql';
+const endpoint = 'http://35.229.114.220/graphql';
 
 const defaultState={
     isLoggedIn: false,
     id:'',
     token:''
 };
-
-export default function reducer(state=defaultState, action){
+async function getData(endpoint,query,variables){
+    var x;
+    await request(endpoint,query,variables).
+	then(data =>{
+	    x=data;
+	});
+    return x;
+}
+export default async function reducer(state=defaultState, action){
     switch(action.type) {
     case 'LOGIN':
 	if(action.username!='' && action.password!=''){
@@ -17,15 +24,21 @@ export default function reducer(state=defaultState, action){
 		email: action.username,
 		password: action.password
 	    };
-
-	    request(endpoint,USER_TOKEN,variables).
-		then(data => console.log(data));
-	    return Object.assign({},state,{
-		isLoggedIn: true,
-		id: action.username,
-		token: action.password
-	    });
-	};
+	    var x= await getData(endpoint,USER_TOKEN,variables);
+	    console.log(x.auth.token);
+	    if(x.auth.token != ''){
+		console.log('entro');
+		return Object.assign({},state,{
+		    isLoggedIn: true,
+		    id: x.auth.id,
+		    token: x.auth.token
+		});
+	    }else{
+		return state;
+	    } 
+	}else{
+	    return state;
+	}
     case 'LOGOUT':
 	return Object.assign({},state,{
 	    isLoggedIn: false,
@@ -35,4 +48,4 @@ export default function reducer(state=defaultState, action){
     default:
 	return state;
     }
-}
+    }
