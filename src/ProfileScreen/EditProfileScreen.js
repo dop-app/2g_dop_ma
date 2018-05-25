fa25ab3304a1cf99add58983985f6e4c080872e0
Juan Caicedo <jecaicedop@unal.edu.co>
2018-05-25 01:37:31 -0500

    View edit profile

--- src/ProfileScreen/EditProfileScreen.js
+++ src/ProfileScreen/EditProfileScreen.js
@@ -1,15 +1,27 @@
 import React  from "react";
 import {AppRegistry, Alert} from "react-native";
 import { connect } from 'react-redux';
-import {Container,Header, Content, Left, Body,Title,Card,CardItem,Right,Icon,Button, Text} from "native-base";
+import {Container,Header, Content, Left, Body,Title,Card,CardItem,Right,Icon,Button, Text,Form, Item, Input,Label} from "native-base";
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 class EditProfileScreen extends React.Component{
+  constructor (props){
+super(props);
+this.state ={
+    route: 'LoginScreen',
+    name: '',
+    email: '',
+    gender: '',
+    age: '',
+    picture:''
+};
+  }
     render() {
 	return (
 	    <Container>
 	      <Header>
 		<Left>
 		  <Button transparent onPress={() => this.props.navigation.goBack() }>
-		    <Icon name="arrow-back"/> 
+		    <Icon name="arrow-back"/>
 		  </Button>
 		</Left>
 		<Body>
@@ -24,9 +36,46 @@ class EditProfileScreen extends React.Component{
 		    <Text>Edit profile</Text>
 		    <Right>
 		      <Icon name="close"/>
-		    </Right>
+        </Right>
 		  </CardItem>
 		</Card>
+    <Form >
+		  <Item floatingLabel>
+		    <Label>Nombre</Label>
+		    <Input
+		      value={this.state.name}
+		      onChangeText={(text) => this.setState({ name: text })} />
+		  </Item>
+		  <Item floatingLabel>
+		    <Label>Email</Label>
+		    <Input
+		      value={this.state.email}
+		      onChangeText={(text) => this.setState({ email: text })}
+		      secureTextEntry={true}/>
+		  </Item>
+      <Item floatingLabel>
+		    <Label>Género</Label>
+		    <Input
+		      value={this.state.gender}
+		      onChangeText={(text) => this.setState({ gender: text })}
+		      secureTextEntry={true}/>
+		  </Item>
+      <Item floatingLabel>
+		    <Label>Edad</Label>
+		    <Input
+		      value={this.state.age}
+		      onChangeText={(text) => this.setState({ age: text })}
+		      secureTextEntry={true}/>
+		  </Item>
+      <Item floatingLabel>
+		    <Label>Foto</Label>
+		    <Input
+		      value={this.state.picture}
+		      onChangeText={(text) => this.setState({ picture: text })}
+		      secureTextEntry={true}/>
+		  </Item>
+		  <Button onPress={(e) => this.userLogin(e)} full style={{ marginTop: 10 }}><Text style={{ fontFamily: 'Comfortaa_regular'}}>Login</Text></Button>
+		</Form>
 	      </Content>
 		</Container>
 	);
