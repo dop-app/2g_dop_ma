c35c55bfb03d28428e00cc0826099b6efe5c4682
oacastillol <oacastillol@unal.edu.co>
2018-05-24 21:39:24 -0500

    stack navigation using dawer navigation

--- src/ProfileScreen/EditScreenOne.js
+++ src/ProfileScreen/EditScreenOne.js
@@ -2,24 +2,20 @@ import React  from "react";
 import {AppRegistry, Alert} from "react-native";
 import {Container,Header, Content, Left, Body,Title,Card,CardItem,Right,Icon,Button, Text} from "native-base";
 export default class EditScreenOne extends React.Component{
-    static navigationOptions = ({navigation}) => ({
-	header: (
-	    <Header>
-	      <Left>
-		<Button transparent onPress={() => navigation.goBack() }>
-		  <Icon name="arrow-back"/> 
-		</Button>
-	      </Left>
-	      <Body>
-		<Title>Editar perfil </Title>
-	      </Body>
-	      <Right />
-	    </Header>
-	)
-    });
     render() {
 	return (
 	    <Container>
+	      <Header>
+		<Left>
+		  <Button transparent onPress={() => this.props.navigation.goBack() }>
+		    <Icon name="arrow-back"/> 
+		  </Button>
+		</Left>
+		<Body>
+		  <Title>Editar perfil </Title>
+		</Body>
+		<Right />
+	      </Header>
 	      <Content padder>
 		<Card>
 		  <CardItem>
