e7664376e5d7d87a2c7a343e12dd2f4da3134a59
Camilo Pinilla <capinillab@unal.edu.co>
2018-05-25 01:16:17 -0500

    Edición presentación gustos, en perfil

--- src/ProfileScreen/Pleasure.js
+++ src/ProfileScreen/Pleasure.js
@@ -1,33 +1,34 @@
 import React  from "react";
-import {Container,Header, Content, Left, Body,Title,Card,CardItem,Right,Icon,Button, Text} from "native-base";
+import {Container,Header, Content, Left, Body,Title,Card,CardItem,Right,Icon,Button, List, ListItem, Text} from "native-base";
 
 export class Pleasure extends React.Component{
-    render(){
-	const {idPleasure,subcategory,name,description} = this.props;
-	return (
-	    <Content>
-	      <Text>{name}</Text>
-	      <Text>{subcategory}</Text>
-	      <Text>{description}</Text>
-	    </Content>
-
-	);
-    }
-    
+  render(){
+		const {idPleasure,subcategory,name,description} = this.props;
+		return (
+		    <Content>
+					<Text>{description} | {subcategory}</Text>
+			    <Text>Nombre: {name}</Text>
+		    </Content>
+		);
+  }
 }
+
 export class PleasureList extends React.Component{
-    render(){
-	const {pleasureList} = this.props;
-	const placeres = pleasureList.map((obj,i)=>(
-	    
-	    <CardItem key={i}>
-	      <Pleasure key={i}  idPleasure={obj.id} name ={obj.name} subcategory={obj.subcategory.name} description={obj.description}/>
-	    </CardItem>
-	));
-	return(
-	    <Card>
+	render(){
+		const {pleasureList} = this.props;
+		const placeres = pleasureList.map((obj,i)=>(
+	    <ListItem key={i}>
+	      <Pleasure key={i}
+					idPleasure={obj.id}
+					name ={obj.name}
+					subcategory={obj.subcategory.name}
+					description={obj.description}/>
+	    </ListItem>
+		));
+		return(
+	    <List>
 	      {placeres}
-	    </Card>	    
-	);
-    }
+	    </List>
+		);
+  }
 }
